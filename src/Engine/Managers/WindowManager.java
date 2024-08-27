ec7a81b9f87efbc08801742ccb659f70c161544e
saperate <61527642+saperate@users.noreply.github.com>
2024-08-27 14:44:32 -0400

    Same thing but cleanup

--- src/Engine/Managers/WindowManager.java
+++ src/Engine/Managers/WindowManager.java
@@ -13,32 +13,30 @@ import java.awt.event.ComponentEvent;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-public class WindowManager extends JFrame  implements WindowListener {
+public class WindowManager extends JFrame implements WindowListener {
     public static WindowManager instance;
     private static final String path = "\\Save\\settings.json";
     private JFrame frame;
-    private boolean fullscreen;
-    private Vector2i location,size;
+    private Vector2i location;
+    private final Vector2i size = new Vector2i(500);
 
-    private WindowManager(){
+    private WindowManager() {
 
     }
 
-    public void init(){
+    public void init() {
         //Load the config file
         JSONObject obj = FileManager.get().getJsonFile(path);
-        if(obj.isEmpty()){
+        if (obj.isEmpty()) {
             genSettings(obj);
         }
         JSONObject gSettings = obj.getJSONObject("graphicSettings");
-        fullscreen = gSettings.getBoolean("fullscreen");
-        location = new Vector2i(gSettings.getInt("locationX"),gSettings.getInt("locationZ"));
-        size = new Vector2i(gSettings.getInt("sizeX"),gSettings.getInt("sizeZ"));
+        location = new Vector2i(gSettings.getInt("locationX"), gSettings.getInt("locationZ"));
 
         initFrame();
     }
 
-    private void initFrame(){
+    private void initFrame() {
         try {
             UIManager.setLookAndFeel(new FlatMacDarkLaf());
         } catch (UnsupportedLookAndFeelException e) {
@@ -46,15 +44,13 @@ public class WindowManager extends JFrame  implements WindowListener {
         }
 
         frame = new JFrame(getTitle());
-        frame.setExtendedState(fullscreen ? JFrame.MAXIMIZED_BOTH : JFrame.NORMAL);
-        frame.setUndecorated(fullscreen);
+        frame.setExtendedState(JFrame.NORMAL);
         frame.setSize(size.getX(), size.getZ());
         frame.setLocation(location.getX(), location.getZ());
         frame.addWindowListener(this);
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 
         Input.get().setFrame(frame);
-        addComponentListener();
 
         frame.setVisible(true);
         frame.setResizable(false);
@@ -64,69 +60,49 @@ public class WindowManager extends JFrame  implements WindowListener {
 
 
     //Might make a settings manager for that but for now there are only graphical settings
-    public void saveSettings(){
+    public void saveSettings() {
         JSONObject obj = new JSONObject();
         JSONObject graphics = new JSONObject();
 
-        graphics.put("fullscreen", fullscreen);
+        graphics.put("locationX", location.getX());
+        graphics.put("locationZ", location.getZ());
 
-        graphics.put("locationX",location.getX());
-        graphics.put("locationZ",location.getZ());
-        graphics.put("sizeX",size.getX());
-        graphics.put("sizeZ",size.getZ());
+        obj.put("graphicSettings", graphics);
 
-        obj.put("graphicSettings",graphics);
-
-        FileManager.get().saveJsonFile(path,obj);
+        FileManager.get().saveJsonFile(path, obj);
     }
 
     //Generates the file with defaults
     //Limitations:
     //--File has to exist
     //--File has to contain {}
-    public void genSettings(JSONObject obj){
+    public void genSettings(JSONObject obj) {
         JSONObject graphics = new JSONObject();
-
-        graphics.put("fullscreen", false);
 
         graphics.put("locationX", 500);
         graphics.put("locationZ", 200);
-        graphics.put("sizeX", 500);
-        graphics.put("sizeZ", 500);
 
-        obj.put("graphicSettings",graphics);
+        obj.put("graphicSettings", graphics);
 
-        FileManager.get().saveJsonFile(path,obj);
+        FileManager.get().saveJsonFile(path, obj);
     }
 
 
-    public String getTitle(){
+    public String getTitle() {
         return "CardStudy";
     }
 
-    public void dispose(){
+    public void dispose() {
         frame.dispose();
     }
 
     public static WindowManager get() {
-        if(instance == null){
+        if (instance == null) {
             instance = new WindowManager();
         }
         return instance;
     }
 
-    public void addComponentListener(){
-        frame.addComponentListener(new ComponentAdapter()
-        {
-            @Override
-            public void componentMoved(ComponentEvent e) {
-                Component c = (Component)e.getSource();
-                if(!fullscreen){
-                    location.set(c.getX(),c.getY());
-                }
-            }
-        });
-    }
 
     @Override
     public void windowOpened(WindowEvent e) {
